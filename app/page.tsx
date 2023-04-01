4b14f63b82cd6b118f61025f50ca96ad82ba7b4a
joshste <josh.steven@ui.ac.id>
2023-04-01 22:54:51 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,154 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const speakers: string[] = [];
+for (let i = 0; i <= 108; i++) {
+  speakers.push(`http://www.localhost:8000/static/samples/${i}.wav`);
+}
 
 export default function Home() {
+  const [filename, setFilename] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [speakerIndex, setSpeakerIndex] = useState("");
+  const [prompt, setPrompt] = useState("");
+
+  const offsetIndex = (offset: number) => {
+    let newIndex = currentIndex + offset;
+    if (newIndex < 0) {
+      newIndex = speakers.length - 1;
+    } else if (newIndex >= speakers.length) {
+      newIndex = 0;
+    }
+    setCurrentIndex(newIndex);
+  };
+
+  const handleSpeakerIndexFieldChange = (event: {
+    target: { value: string };
+  }) => {
+    const result = event.target.value.replace(/\D/g, "");
+
+    setSpeakerIndex(result);
+  };
+
+  const changeSpeakerIndex = () => {
+    let index = Number(speakerIndex);
+    if (index < 0) index = 0;
+    else if (index >= speakers.length) index = speakers.length - 1;
+    setCurrentIndex(index);
+    setSpeakerIndex(index.toString());
+  };
+
+  const playAudio = () => {
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+    audio?.play();
+  };
+
+  useEffect(() => {
+    setAudio((prev) => {
+      if (prev) {
+        prev.pause();
+        prev.currentTime = 0;
+      }
+      return new Audio(speakers[currentIndex]);
+    });
+  }, [currentIndex]);
+
+  useEffect(() => {
+    audio?.play();
+  }, [audio]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <form className="w-1/3">
+        <div className="mb-4">
+          <label htmlFor="prompt" className="block font-bold mb-1">
+            Your text:
+          </label>
+          <textarea
+            id="prompt"
+            name="prompt"
+            value={prompt}
+            onChange={(event) => setPrompt(event.target.value)}
+            required
+            className="w-full px-3 py-2 border rounded-md border-gray-400"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="speaker-index" className="block font-bold mb-1">
+            Speaker Index:
+          </label>
+
+          <div id="speaker-selection" className="relative w-48 h-16">
+            <input
+              type="text"
+              size={1}
+              id="speaker-index"
+              value={speakerIndex}
+              onChange={handleSpeakerIndexFieldChange}
+              className="w-full px-3 py-2 border rounded-md border-gray-400"
             />
-          </a>
+            <a href="#" onClick={changeSpeakerIndex}>
+              Go
+            </a>
+
+            <div className="">
+              <a
+                href="#"
+                className="" // make this in the left positions
+                onClick={() => {
+                  offsetIndex(-1);
+                }}
+              >
+                &#10094;
+              </a>
+              <div className="">{currentIndex}</div>
+              <audio id="audio-sample"></audio>
+              <a href="#" className="" onClick={playAudio}>
+                &#9654;
+              </a>
+              <a
+                href="#"
+                className=""
+                onClick={() => {
+                  offsetIndex(1);
+                }}
+              >
+                &#10095;
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div className="mb-4 flex flex-row items-center justify-start max-w-sm gap-8">
+          <label htmlFor="filename" className="block font-bold mb-1">
+            Filename:
+          </label>
+          <div className="flex flex-row items-center">
+            <input
+              type="text"
+              size={7}
+              id="filename"
+              name="filename"
+              value={filename}
+              onChange={(event) => setFilename(event.target.value)}
+              required
+              className="w-full px-3 py-2 border rounded-md border-gray-400"
+            />
+            <div>.wav</div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
